7d471083c1bb68e11e34839b1a696c8cd8015f20
Rodney Osodo <socials@rodneyosodo.com>
2023-09-03 21:40:55 +0300

    Fix max_gpt_tokens assignment in GPT class
    The max_gpt_tokens attribute was not being assigned correctly in the GPT class. This commit fixes the issue by correctly assigning the value provided in the diff.
    This is to avoid passing huge git diff to openai
    
    Signed-off-by: Rodney Osodo <socials@rodneyosodo.com>

--- commitgpt/gpt.py
+++ commitgpt/gpt.py
@@ -20,6 +20,7 @@ class GPT:
         self.max_tokens = 256
         self.n = 1
         self.stop = None
+        self.max_gpt_tokens = 2048
 
     def api_key(self, api_key: str) -> None:
         """api_key sets the openai api key.
@@ -50,6 +51,9 @@ class GPT:
         prompt = COMMIT_PROMPT.replace(
             "{git_diff}", git_dif).replace(
             "{commit_guidelines}", guidelines)
+
+        if len(prompt) > self.max_gpt_tokens:
+            prompt = prompt[:self.max_gpt_tokens - 100]
 
         reponse = openai.ChatCompletion.create(
             model=self.model,
